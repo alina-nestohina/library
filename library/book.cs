b6d25af1e57b31f1b5c1f11c1a6871fae42f5f10
Нестьогіна Аліна <alina.nestohina@nure.ua>
2026-05-05 21:19:40 +0200

    Successfully added all library source files

---
+++ library/book.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// Клас, що описує книгу в бібліотеці.
+/// </summary>
+public class Book
+{
+    /// <summary> Назва книги. </summary>
+    public string Title { get; set; }
+    
+    /// <summary> Автор книги. </summary>
+    public string Author { get; set; }
+    
+    /// <summary> Рік видання. </summary>
+    public int Year { get; set; }
+}
